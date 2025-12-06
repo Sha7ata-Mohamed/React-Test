c6007966afed4df428a81e6e7464fcb1b9e8794b
Sha7ata-Mohamed <sha7ata1379@gmail.com>
2025-12-06 03:22:37 +0300

    d
    d

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,27 @@
+import MovieCard from "../components/MovieCard"
+
+function Home() {
+    const movies = [
+        { id: 1, title: "Inception", release_date: "2010" },
+        { id: 2, title: "The Dark Knight", release_date: "2008" },
+        { id: 3, title: "Interstellar", release_date: "2014" },
+    ]
+    const handleSearch = () => {
+
+    };
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for movies..." className="search-input" />
+            </form>
+            <button type="submit" className="search-button">search</button>
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home
